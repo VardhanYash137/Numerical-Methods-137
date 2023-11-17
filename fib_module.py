5ae39bd1c830a50a38698a44153cdee8f5d242fb
Yash Vardhan <135862777+VardhanYash137@users.noreply.github.com>
2023-11-17 11:43:30 +0530

    Update fib_module.py

--- fib_module.py
+++ fib_module.py
@@ -1,9 +1,1 @@
 import numpy as np
-
-def fib_series(n):
-    ans_list=np.ones((n))
-    for i in range(2,n):
-        ele=ans_list[i-1]+ans_list[i-2]
-        ans_list[i]=ele
-    return ans_list
-
